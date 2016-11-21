b4e4004b73cb9acacc63574ffa0f0a00976e9d90
shengfeng.dong <shengfeng.dong@szlegate.com>
2016-11-21 13:52:35 +0800

    Signed-off-by: shengfeng.dong <shengfeng.dong@szlegate.com>

---
+++ 3code/LinuxEnvironment/uc_day01/1error.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define VERSION 3
+
+#if (VERSION<3)
+//	#error "版本太低！"
+	#warning "版本太低！"
+#elif (VERSION>3)
+//	#error "版本太高！"
+	#warning "版本太高！"
+#endif
+
+int main()
+{
+	printf("版本:%d\n",VERSION);
+	return 0;
+}
